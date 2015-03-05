f9e117b578fd30ea9d7fd3b89bf0ba45ed994237
Gerry Salazar <gs@hspindesign.com>
2015-03-05 10:03:03 -0500

    fixed cifs-utils

--- 02-com-install.sh
+++ 02-com-install.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y xchat
 sudo apt-get install -y xscreensaver
 sudo apt-get install -y tty-clock
 sudo apt-get install -y autojump
-sudo apt-get install -y cifs-util
+sudo apt-get install -y cifs-utils
 sudo apt-get install -y cryptsetup-bin
 
 sudo apt-get install -y firefox
